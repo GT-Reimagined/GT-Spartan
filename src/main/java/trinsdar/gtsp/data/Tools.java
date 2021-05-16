af6d16b6de382933d6d181d487a3ac9b16adac3f
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-05-16 01:09:03 -0400

    material item part stuff

--- src/main/java/trinsdar/gtsp/data/Tools.java
+++ src/main/java/trinsdar/gtsp/data/Tools.java
@@ -1,10 +1,14 @@
 package trinsdar.gtsp.data;
 
 import com.oblivioussp.spartanweaponry.api.WeaponTraits;
+import com.oblivioussp.spartanweaponry.init.ModItems;
+import muramasa.antimatter.AntimatterAPI;
+import muramasa.antimatter.Data;
 import muramasa.antimatter.material.Material;
 import muramasa.antimatter.material.MaterialItem;
 import muramasa.antimatter.material.MaterialType;
 import muramasa.antimatter.material.MaterialTypeItem;
+import muramasa.antimatter.material.TextureSet;
 import muramasa.antimatter.tool.AntimatterToolType;
 import muramasa.antimatter.util.TagUtils;
 import muramasa.antimatter.util.Utils;
@@ -16,20 +20,23 @@ import trinsdar.gtsp.items.MaterialItemToolPart;
 import trinsdar.gtsp.items.MaterialSwordBig;
 import trinsdar.gtsp.tool.GTSPToolType;
 
+import static com.google.common.collect.ImmutableMap.of;
+import static muramasa.antimatter.Data.PLATE;
+
 
 public class Tools {
 
     public static MaterialTypeItem<?> POLE = new MaterialTypeItem<>("pole", 1, true, muramasa.antimatter.Ref.U * 2, new MaterialTypeItem.ItemSupplier() {
         @Override
         public MaterialItem supply(String domain, MaterialType<?> type, Material material) {
-            return new MaterialItemToolPart(domain, type, material, new Item.Properties().group(muramasa.antimatter.Ref.TAB_MATERIALS));
+            return new MaterialItemToolPart(Ref.ID, type, material, new Item.Properties().group(muramasa.antimatter.Ref.TAB_MATERIALS));
         }
     });
 
     public static MaterialTypeItem<?> HANDLE = new MaterialTypeItem<>("handle_sp", 1, true, muramasa.antimatter.Ref.U * 2, new MaterialTypeItem.ItemSupplier() {
         @Override
         public MaterialItem supply(String domain, MaterialType<?> type, Material material) {
-            return new MaterialItemToolPart(domain, type, material, new Item.Properties().group(muramasa.antimatter.Ref.TAB_MATERIALS));
+            return new MaterialItemToolPart(Ref.ID, type, material, new Item.Properties().group(muramasa.antimatter.Ref.TAB_MATERIALS));
         }
 
         @SuppressWarnings("unchecked")
@@ -45,8 +52,14 @@ public class Tools {
     public static AntimatterToolType KATANA = new GTSPToolType(Ref.ID, "katana", 2, 1, 10, 1.5F, -2.0F, WeaponTraits.TWO_HANDED_1, WeaponTraits.EXTRA_DAMAGE_2_CHEST, WeaponTraits.REACH_1).setToolClass(MaterialSwordBig.class).addEffectiveBlocks(Blocks.COBWEB).addToolTypes("sword");
     public static AntimatterToolType LONGSWORD = new GTSPToolType(Ref.ID, "longsword", 2, 1, 10, 4.5F, -2.6F, WeaponTraits.TWO_HANDED_1, WeaponTraits.SWEEP_DAMAGE_NORMAL).setToolClass(MaterialSwordBig.class).addEffectiveBlocks(Blocks.COBWEB).addToolTypes("sword");
     public static AntimatterToolType SABER = new GTSPToolType(Ref.ID, "saber", 2, 1, 10, 1.5F, -2.4F, WeaponTraits.DAMAGE_ABSORB_25, WeaponTraits.EXTRA_DAMAGE_2_CHEST, WeaponTraits.SWEEP_DAMAGE_NORMAL).setToolClass(MaterialSwordBig.class).addEffectiveBlocks(Blocks.COBWEB).addToolTypes("sword");
+    public static Material Wood = new Material(Ref.ID, "wood", 0x643200, TextureSet.NONE).asDust(PLATE).addHandleStat(12, 0.0F);
 
     public static void init(){
-
+        Material wood = Material.get("wood");
+        if (wood != Data.NULL){
+            HANDLE.forceOverride(wood, ModItems.handle);
+            POLE.forceOverride(wood, ModItems.pole);
+        }
+        Data.Stone.flags(HANDLE, POLE);
     }
 }
--- src/main/java/trinsdar/gtsp/items/MaterialItemToolPart.java
+++ src/main/java/trinsdar/gtsp/items/MaterialItemToolPart.java
@@ -1,17 +1,28 @@
 package trinsdar.gtsp.items;
 
+import com.oblivioussp.spartanweaponry.api.SpartanWeaponryAPI;
 import muramasa.antimatter.material.Material;
 import muramasa.antimatter.material.MaterialItem;
 import muramasa.antimatter.material.MaterialType;
 import muramasa.antimatter.texture.Texture;
 import trinsdar.gtsp.Ref;
 
-public class MaterialItemToolPart extends MaterialItem {
+import static trinsdar.gtsp.data.Tools.POLE;
+
+public class MaterialItemToolPart extends MaterialItem implements IMaterialItemSpartan {
     public MaterialItemToolPart(String domain, MaterialType<?> type, Material material, Properties properties) {
         super(domain, type, material, properties);
     }
 
     public Texture[] getTextures() {
         return new Texture[]{new Texture(Ref.ID, "material/" + type.getId()), new Texture(Ref.ID, "material/" + type.getId() + "_overlay")};
+    }
+
+    @Override
+    public String getParent() {
+        if (type == POLE){
+            return SpartanWeaponryAPI.MOD_ID + ":item/pole";
+        }
+        return "minecraft:item/handheld";
     }
 }
