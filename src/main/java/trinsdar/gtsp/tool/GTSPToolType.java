3736a4a0d6ee78789ca2536bc2992b9d135adca2
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-12-07 18:11:41 -0500

    added more weapons

--- src/main/java/trinsdar/gtsp/tool/GTSPToolType.java
+++ src/main/java/trinsdar/gtsp/tool/GTSPToolType.java
@@ -1,25 +1,16 @@
 package trinsdar.gtsp.tool;
 
 import com.oblivioussp.spartanweaponry.api.SpartanWeaponryAPI;
-import com.oblivioussp.spartanweaponry.api.trait.WeaponTrait;
 import com.oblivioussp.spartanweaponry.util.WeaponArchetype;
-import muramasa.antimatter.AntimatterAPI;
+import muramasa.antimatter.data.AntimatterDefaultTools;
 import muramasa.antimatter.data.AntimatterMaterials;
-import muramasa.antimatter.material.Material;
-import muramasa.antimatter.tool.AntimatterItemTier;
 import muramasa.antimatter.tool.AntimatterToolType;
-import muramasa.antimatter.tool.IAntimatterTool;
 import muramasa.antimatter.util.TagUtils;
-import muramasa.antimatter.util.Utils;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item.Properties;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.level.block.Blocks;
 import trinsdar.gtsp.items.MaterialSwordSpartan;
 
-import java.util.Objects;
-import java.util.function.Supplier;
-
-public class GTSPToolType  extends AntimatterToolType {
+public class GTSPToolType extends AntimatterToolType {
 
     private final WeaponArchetype archetype;
 
@@ -33,31 +24,9 @@ public class GTSPToolType  extends AntimatterToolType {
             if (tier.getPrimary() == AntimatterMaterials.NetherizedDiamond) properties.fireResistant();
             return new MaterialSwordSpartan(domain2, toolType, tier, properties, archetype, weaponDamageMultiplier);
         });
-        String tagString = id.endsWith("s") ? id : id + "s";
+        String tagString = id.equals("quarterstaff") ? "quarterstaves" : id.endsWith("s") ? id : id + "s";
         this.tag = TagUtils.getItemTag(new ResourceLocation(SpartanWeaponryAPI.MOD_ID, tagString));
+        this.addEffectiveBlocks(Blocks.COBWEB).setType(AntimatterDefaultTools.SWORD);
+        if (id.equals("battleaxe")) addTags("axe");
     }
-
-    /*@Override
-    public IAntimatterTool instantiateTools(String domain) {
-        if (this.getToolClass() == MaterialSwordSp.class) return new MaterialSwordSp(domain, this, prepareInstantiation(domain));
-        return super.instantiateTools(domain);
-    }
-
-    @Override
-    public IAntimatterTool instantiateTools(String domain, Supplier<Item.Properties> properties) {
-        if (this.getToolClass() == MaterialSwordSp.class) return new MaterialSwordSp(domain, this, properties.get());
-        return super.instantiateTools(domain, properties);
-    }
-
-    private Item.Properties prepareInstantiation(String domain) {
-        if (domain.isEmpty()) Utils.onInvalidData("An AntimatterToolType was instantiated with an empty domain name!");
-        Item.Properties properties = new Item.Properties().group(getItemGroup());
-        if (!getRepairability()) properties.setNoRepair();
-        return properties;
-    }
-
-    @Override
-    public ItemStack getToolStack(Material primary, Material secondary) {
-        return Objects.requireNonNull(AntimatterAPI.get(IAntimatterTool.class, getId(), getDomain())).asItemStack(primary, secondary);
-    }*/
 }
